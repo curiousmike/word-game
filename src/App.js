09c9c2699f49a5ceecc1fb84ee8b74205bca5176
Michael Coustier <curiousmike@gmail.com>
2020-09-06 13:14:36 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import WordInput from "./components/wordInput/wordInput.component";
+import WordList from "./components/wordList/wordList.component";
+import Button from "./components/button/button.component";
+import Board from "./components/board/board.component";
+import { Solver } from "./solver.js";
 
 function App() {
+  const [words, setWords] = useState([]);
+  const [letterInput, setLetters] = useState("");
+  const [minWordLength, setMinWordLength] = useState(0);
+  const [maxWordLength, setMaxWordLength] = useState(0);
+
+  function solve() {
+    handleSolveClick();
+  }
+
+  const handleSolveClick = React.useCallback(() => {
+    const words = Solver(letterInput);
+    setWords(words);
+
+    var min = null;
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].length < min || !min) {
+        min = words[i].length;
+      }
+    }
+    if (!min) {
+      min = 2;
+    }
+    setMinWordLength(min);
+
+    var max = null;
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].length > max || !max) {
+        max = words[i].length;
+      }
+    }
+    if (!max) {
+      max = 2;
+    }
+    setMaxWordLength(max);
+    console.log("Total words found = ", words.length);
+  }, [letterInput, setWords]);
+
+  function handleKeyPress(e) {
+    setLetters(e.target.value);
+    if (e.key === "Enter") {
+      handleSolveClick();
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    handleSolveClick();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="Board">
+        <Board />
+      </div>
+      <div class="Tools">
+        <WordInput
+          value={letterInput}
+          onKeyPress={handleKeyPress}
+          onSubmit={handleSubmit}
+        />
+        <Button text="solve" onClick={solve} />
+        <WordList words={words} />
+      </div>
     </div>
   );
 }
