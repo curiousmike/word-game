e00091305712a32386014c37639a1bb8f2cc6719
Michael Coustier <curiousmike@gmail.com>
2020-09-06 20:06:15 -0700

    keyboard working. place on map working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import WordInput from "./components/wordInput/wordInput.component";
 import WordList from "./components/wordList/wordList.component";
 import Button from "./components/button/button.component";
 import Board from "./components/board/board.component";
 import { Solver } from "./solver.js";
+const boardDimensions = 10;
 
 function App() {
   const [words, setWords] = useState([]);
@@ -14,6 +15,21 @@ function App() {
   const [selectedWord, setSelectedWord] = useState(null);
   const [hoverRow, setHoverRow] = useState(0);
   const [hoverColumn, setHoverColumn] = useState(0);
+  const [wordDirection, setWordDirection] = useState("horizontal");
+  const [mapDetails, setMapDetails] = useState([boardDimensions]);
+
+  // for useEffect, if second argument is empty array it behaves like componentDidMount *only*
+  useEffect(() => {
+    // code to run on component mount
+    let nullBoard = [boardDimensions];
+    for (let row = 0; row < boardDimensions; row++) {
+      nullBoard[row] = Array(boardDimensions);
+      for (let col = 0; col < boardDimensions; col++) {
+        nullBoard[row][col] = "*";
+      }
+    }
+    setMapDetails(nullBoard);
+  }, []);
 
   function solve() {
     handleSolveClick();
@@ -59,10 +75,33 @@ function App() {
 
   function tileClick(e) {
     const value = e.target.getAttribute("id").split(",");
+    const startcol = parseInt(e.target.getAttribute("startcol"));
+    const startrow = parseInt(e.target.getAttribute("startrow"));
+
     const row = parseInt(value[0]);
     const col = parseInt(value[1]);
+
     setSelectedCol(col);
     setSelectedRow(row);
+
+    let nullBoard = [boardDimensions];
+    for (let row = 0; row < boardDimensions; row++) {
+      nullBoard[row] = Array(boardDimensions);
+      for (let col = 0; col < boardDimensions; col++) {
+        nullBoard[row][col] = mapDetails[row][col];
+      }
+    }
+
+    if (wordDirection === "vertical") {
+      for (let i = 0; i < selectedWord.length; i++) {
+        nullBoard[startrow + i][startcol] = selectedWord[i];
+      }
+    } else {
+      for (let i = 0; i < selectedWord.length; i++) {
+        nullBoard[startrow][startcol + i] = selectedWord[i];
+      }
+    }
+    setMapDetails(nullBoard);
   }
 
   function mouseOver(e) {
@@ -78,20 +117,41 @@ function App() {
   }
 
   function handleGlobalKeyPress(e) {
-    console.log("keypress e = ", e);
-    // if e.value == 'space' wordDirection = otherDirection;
+    if (e.key === " ") {
+      if (wordDirection === "vertical") {
+        setWordDirection("horizontal");
+      } else {
+        setWordDirection("vertical");
+      }
+    }
   }
+
+  function handleDirectionFlip(e) {
+    e.preventDefault();
+    if (wordDirection === "vertical") {
+      setWordDirection("horizontal");
+    } else {
+      setWordDirection("vertical");
+    }
+  }
+
   return (
     <div className="App">
-      <div className="Board">
+      <div
+        className="Board"
+        tabIndex="0"
+        onContextMenu={handleDirectionFlip}
+        onKeyPress={handleGlobalKeyPress}
+      >
         <Board
           onClick={tileClick}
           onMouseOver={mouseOver}
+          onKeyDown={handleGlobalKeyPress}
           selectedWord={selectedWord}
           hoverColumn={hoverColumn}
           hoverRow={hoverRow}
-          wordDirection="vertical"
-          onKeyDown={handleGlobalKeyPress}
+          wordDirection={wordDirection}
+          boardDetails={mapDetails}
         />
       </div>
       <div className="Tools">
